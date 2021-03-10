0ff71a8b9b5d26fe69f76e594ad38baf9f2d82b8
kouvang322 <kouvang322@hotmail.com>
2021-03-10 17:16:02 -0600

    slight changes
    removed some comments, got new photo, made signup form have picture background

--- signup.js
+++ signup.js
@@ -81,37 +81,3 @@ $('.memberPictures > div').mouseleave(function(){
 
 
 )
-
-// $(document).ready(function(){
-//     $("p").hide();
-   
-
-// //Now I want to assign the targeted element's value to the variable I just created
-// var userEmail =  $('#Email').val();
-
-// console.log(userEmail);
-
-
-
-// //FUNCTION
-// $('#submitter').click(function(){
-
-//     var userEmail =  $('#Email').val();
-    
-
-
-//     //if it's empty have an alert
-//     if (!userEmail || !(userEmail.indexOf("@") > -1)){
-//         $(".errorEmail").slideDown();
-//         return;
-//     }
-    
- 
-//     //At this point we can assume that it is NOT empty AND it includes a @
-//     alert("Congrats , you've sign-up with email: " + userEmail)
-
-// })
-
-
-
-// })
